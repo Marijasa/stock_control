6786fe5939e7821309b81882a94120ae50b13ab5
Mariela Jara Sanchez -X (mjarasan - DARWIN ZONE SA at Cisco) <mjarasan@cisco.com>
2024-10-06 21:06:01 -0600

    total price in product list

--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 import ProductService from '../../services/Product.service';
+import useFormatCurrency from "../../useFormatCurrency";
+import {useSelector} from "react-redux";
 
 const ProductList = () => {
     const [products, setProducts] = useState([]);
+    const [total_original, setTotalOriginal] = useState(0);
+    const [total_price, setTotalPrice] = useState(0);
+
+    const dollarPrice = useSelector(state => state.dollar.data);
+
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -16,9 +23,31 @@ const ProductList = () => {
             });
     }, []);
 
+    useEffect(() => {
+        let auxOriginal = 0;
+        let auxPrice = 0;
+
+        products.map((product) => {
+            auxOriginal += Number(product.original_price);
+            auxPrice += Number(product.price);
+            return product;
+        });
+
+        setTotalOriginal(auxOriginal);
+        setTotalPrice(auxPrice);
+
+    }, [products, total_original, total_price]);
+
     return (
         <div className="container mt-5">
             <h1 className="mb-4">Product List</h1>
+            <div className="row mb-3">
+                <h3 className={'col-3'}>Total Original Price Dolar: {useFormatCurrency(total_original, 'USD')}</h3>
+                <h3 className={'col-3 border-end border-3'}>Total Original Price Colon: {useFormatCurrency(total_original * dollarPrice.venta, 'CRC')}</h3>
+
+                <h3 className={'col-3'}>Total Price Dolar: {useFormatCurrency(total_price, 'USD')}</h3>
+                <h3 className={'col-3'}>Total Price Colon: {useFormatCurrency(total_price * dollarPrice.venta, 'CRC')}</h3>
+            </div>
             <Link to="/products/new" className="btn btn-primary mb-4">Add Product</Link>
             <table className="table table-striped table-hover">
                 <thead className="thead-dark">
@@ -45,8 +74,10 @@ const ProductList = () => {
                             <Link to={`/products/${product.id}`} className="btn btn-info btn-sm me-2">View</Link>
                             <Link to={`/products/edit/${product.id}`}
                                   className="btn btn-warning btn-sm me-2">Edit</Link>
-                            <Link to={`/products/delete/${product.id}`} className="btn btn-danger btn-sm me-2">Delete</Link>
-                            <a className={'btn btn-secondary btn-sm ' + (product.instagram_url !== null ? '' : 'disabled')} rel={'noreferrer'} target={'_blank'} href={product.instagram_url}>Instagram</a>
+                            <Link to={`/products/delete/${product.id}`}
+                                  className="btn btn-danger btn-sm me-2">Delete</Link>
+                            <a className={'btn btn-secondary btn-sm ' + (product.instagram_url !== null ? '' : 'disabled')}
+                               rel={'noreferrer'} target={'_blank'} href={product.instagram_url}>Instagram</a>
                         </td>
                     </tr>
                 ))}
